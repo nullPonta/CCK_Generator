be22965c06ecd376de4af976755e151c029d4f63
nullponta <null@ponta.com>
2020-12-13 14:06:12 +0900

    LogicParamWrapperへ便利関数を定義

---
+++ Assets/CCK_Generator/Eidtor/Base/LogicParamWrapper.cs
@@ -0,0 +1,23 @@
+﻿using ClusterVR.CreatorKit;
+using ClusterVR.CreatorKit.Gimmick;
+using ClusterVR.CreatorKit.Operation;
+
+
+namespace Ponta.CCK_Generator.Base {
+
+    public class LogicParamWrapper
+    {
+
+        public static SingleStatement SendSignalToSelf(Operator inOperator, string key, string sendKey) {
+
+            var sendSignal = LogicParamGenerator.CreateSingleStatement(
+                        LogicParamGenerator.CreateExpression_IF(inOperator, GimmickTarget.Item, key),
+                        new Base.TargetState(TargetStateTarget.Item, sendKey, ParameterType.Signal));
+
+            return sendSignal;
+        }
+
+    }
+
+}
+
--- Assets/CCK_Generator/Eidtor/HandGunCreator.cs
+++ Assets/CCK_Generator/Eidtor/HandGunCreator.cs
@@ -43,7 +43,9 @@ namespace Ponta.CCK_Generator
             prefabsPathController.OutputPath = "HandGun.prefab";
             prefabsPathController.PrototypePath = "Prototype_HandGun.prefab";
 
+            /* ---------------------------------------------------------------- */
             /* Define : Item  */
+            /* ---------------------------------------------------------------- */
             itemInfo.isItem = true;
             itemInfo.itemName = "ハンドガン";
 
@@ -51,44 +53,65 @@ namespace Ponta.CCK_Generator
 
             itemInfo.isGrabbableItem = true;
 
-            /* Define : OnCreateItemTrigger */
+            /* ---------------------------------------------------------------- */
+            /* Define : Trigger  */
+            /* ---------------------------------------------------------------- */
+
+            /* OnCreateItemTrigger */
             var bullets = triggerInfo.CreateTriggerParamInteger(TriggerTarget.Item, null, "bullets", 6);
             var maxBullets = triggerInfo.CreateTriggerParamInteger(TriggerTarget.Item, null, "maxBullets", 6);
 
             triggerInfo.AddOnCreateItemTrigger(bullets);
             triggerInfo.AddOnCreateItemTrigger(maxBullets);
 
-            /* Define : OnGrabItemTrigger */
+            /* OnGrabItemTrigger */
             var enableUI_On = triggerInfo.CreateTriggerParamBool(TriggerTarget.Item, null, "enableUI", true);
             var reloaded = triggerInfo.CreateTriggerParamSignal(TriggerTarget.Item, null, "Reloaded");
 
             triggerInfo.AddOnGrabItemTrigger(enableUI_On);
             triggerInfo.AddOnGrabItemTrigger(reloaded);
 
-            /* Define : OnReleaseItemTrigger */
+            /* OnReleaseItemTrigger */
             var enableUI_Off = triggerInfo.CreateTriggerParamBool(TriggerTarget.Item, null, "enableUI", false);
 
             triggerInfo.AddOnReleaseItemTrigger(enableUI_Off);
 
-            /* Define : UseItemTrigger */
+            /* UseItemTrigger */
             var shootUnlessReloading = triggerInfo.CreateTriggerParamSignal(TriggerTarget.Item, null, "ShootUnlessReloading");
             triggerInfo.AddUseItemTrigger_Down(shootUnlessReloading);
 
-            /* Define : ItemLogic */
+            /* ---------------------------------------------------------------- */
+            /* Define : Item  */
+            /* ---------------------------------------------------------------- */
+
+            /* ItemLogic */
             {
                 /* On receive */
                 var onReceive = LogicParamGenerator.CreateOnReceiveKey(GimmickTarget.Item, "ShootUnlessReloading");
 
                 /* Logic */
                 // if (!reloading) { SendSignal(Item, "ShootOrReload") }
-                var sendSignal = LogicParamGenerator.CreateSingleStatement(
-                        LogicParamGenerator.CreateExpression_IF(Operator.Not, GimmickTarget.Item, "reloading"),
-                        new Base.TargetState(TargetStateTarget.Item, "ShootOrReload", ParameterType.Signal));
-
+                var sendSignal = LogicParamWrapper.SendSignalToSelf(Operator.Not, "reloading", "ShootOrReload");
                 var logic = LogicParamGenerator.CreateLogic_AtSingleStatement(sendSignal);
 
                 /* LogicParam */
                 logicInfo.AddItemLogicParam(new LogicParam(onReceive, logic));
+            }
+
+            {
+                /* On receive */
+                var onReceive = LogicParamGenerator.CreateOnReceiveKey(GimmickTarget.Item, "ShootOrReload");
+
+                /* Logic */
+                // if (bullets > 0) { SendSignal(Item, "Shoot") }
+                // if (bullets <= 0) { SendSignal(Item, "Reload") }
+                var sendShootSignal = LogicParamGenerator.CreateSingleStatement(
+                        LogicParamGenerator.CreateExpression_IF(Operator.GreaterThan, GimmickTarget.Item, "bullets"),
+                        new Base.TargetState(TargetStateTarget.Item, "Shoot", ParameterType.Signal));
+
+
+                /* LogicParam */
+
             }
 
         }
